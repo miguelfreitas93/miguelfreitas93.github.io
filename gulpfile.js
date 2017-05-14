c8702284229d8cc0b99a8770abc0f72fcf100c9d
miguelfreitas93 <miguel_freitas93@hotmail.com>
2017-05-14 02:39:27 +0100

    Website First commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp"),
+    browserSync = require('browser-sync').create(),
+    sass = require("gulp-sass"),
+    imagemin = require('gulp-imagemin');
+
+gulp.task('images', ['min-images'])
+
+gulp.task('min-images', function () {
+    return gulp.src('images/*.{png,gif,jpg,jpeg}')
+        .pipe(imagemin({optimizationLevel: 5, progressive: true, interlaced: true}))
+        .pipe(gulp.dest('images/'));
+});
+
+gulp.task("sass", function() {
+  return gulp.src('./css/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task("watch", function() {
+  gulp.watch('./css/**/*.scss',['sass']);
+});
+
+gulp.task('init-server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('dev', ['sass','watch','init-server']);
